bd50de80e4543e19673f84ce415b9deb2ed9a507
Jhofran Briceno <jhonfran.briceno@novustech.la>
2022-05-06 14:26:50 -0500

    0.1.3V

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from 'react';
-import './App.css';
-import EditorMain from './components/EditorMain';
+import TestComponent from './components/TestComponent';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <div>
-        <EditorMain />
+        <TestComponent />
       </div>
     </div>
   )
