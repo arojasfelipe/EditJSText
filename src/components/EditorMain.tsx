cfe2b8cd909fba6620b7ceaf7bd072888b56600f
unknown <you@example.com>
2022-05-06 12:38:27 -0500

    configuration initial

---
+++ src/components/EditorMain.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import EditorJS from '@editorjs/editorjs';
+
+function EditorMain() {
+    return (
+        <div>
+            <h3>Editor</h3>
+        </div>
+    )
+}
+
+export default EditorMain
